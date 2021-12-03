8f58f97486a68397ffe9f8b4c673eb40220f926a
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-03 16:01:26 -0600

    hey, its me again

---
+++ src/main/java/com/resume/Jeremiah/controllers/ContactController.java
@@ -0,0 +1,12 @@
+package com.resume.Jeremiah.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+    @GetMapping("/contact")
+    public String welcome() {
+        return "contact";
+    }
+}
