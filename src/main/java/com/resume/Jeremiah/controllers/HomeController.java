005bf39f72b64c7f4dcdcf15e4773ee476bb5521
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-03 14:40:04 -0600

    home and template creation

--- src/main/java/com/resume/Jeremiah/controllers/HomeController.java
+++ src/main/java/com/resume/Jeremiah/controllers/HomeController.java
@@ -1,4 +1,12 @@
 package com.resume.Jeremiah.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
 }
