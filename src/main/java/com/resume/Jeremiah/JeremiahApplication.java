2a730f7363cb7f5a2a571b60b09dcd4fe64cc55b
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-03 12:36:41 -0600

    initial

---
+++ src/main/java/com/resume/Jeremiah/JeremiahApplication.java
@@ -0,0 +1,13 @@
+package com.resume.Jeremiah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeremiahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeremiahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resume/Jeremiah/JeremiahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.Jeremiah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeremiahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
