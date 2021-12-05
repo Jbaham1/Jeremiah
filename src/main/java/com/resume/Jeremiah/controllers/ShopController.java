327bcf643dc8004a9881ea0d438efe5e0720b788
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-04 23:19:30 -0600

    ecomm  setup without the models and repo

---
+++ src/main/java/com/resume/Jeremiah/controllers/ShopController.java
@@ -0,0 +1,12 @@
+package com.resume.Jeremiah.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+    @GetMapping("/shop")
+    public String welcome() {
+        return "shop/home";
+    }
+}
